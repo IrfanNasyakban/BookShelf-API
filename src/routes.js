8b390c3afde160d2171ae5ccef923a92a42e2072
IrfanNasyakban <irfanna.syakban098@gmail.com>
2022-11-03 11:52:45 +0700

    bookshelf api

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const {
+    tambahBuku,
+    tampilBuku,
+    tampilBukuId,
+    editBuku,
+    hapusBuku
+  } = require('./handler');
+  
+  const routes = [
+    {
+      method: 'POST',
+      path: '/books',
+      handler: tambahBuku,
+    },
+    {
+      method: 'GET',
+      path: '/books',
+      handler: tampilBuku,
+    },
+    {
+      method: 'GET',
+      path: '/books/{bookId}',
+      handler: tampilBukuId,
+    },
+    {
+      method: 'PUT',
+      path: '/books/{bookId}',
+      handler: editBuku,
+    },
+    {
+      method: 'DELETE',
+      path: '/books/{id}',
+      handler: hapusBuku,
+    },
+  ];
+  
+  module.exports = routes;
